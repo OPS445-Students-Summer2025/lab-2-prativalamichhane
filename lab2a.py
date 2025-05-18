b2e67c943f1302fc135a97d9aa480b3dae474891
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-05-18 01:39:28 -0400

    fixed lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-name = 'Jon'
+name = "Jon"
 age = 20
-print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+print("Hi " + name + ", you are " + str(age) + " years old.")
 
