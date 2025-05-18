19277220d1b1132d902c669ea5675982d0f4d1a8
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-05-18 01:33:57 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author: Prativa Lamichhane
+#Author ID: 167520220
+#Date Created: 2025/05/18
+timer = 10
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
+
+
